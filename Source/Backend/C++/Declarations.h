0da31736a79003793816cfc9a156176c2e918d3b
marc-rene <95175463+marc-rene@users.noreply.github.com>
2022-10-01 23:47:16 +0100

    Add files via upload

--- Source/Backend/C++/Declarations.h
+++ Source/Backend/C++/Declarations.h
@@ -1,87 +1,178 @@
 #include        		<stdio.h>
 #include        		<stdlib.h>
-#include        		<iostream>
-#include        		<vector>
-#include        		<thread>
-#include        		<string>
-#include        		<fstream>
-#include				<algorithm>
+#include				<time.h>
+#include				<string.h>
+#include				<string>
+#include				<iostream>
+#include				<fstream>
+#include 				<thread>
 
 
-#define         	mint                        	__int8           	//	micro int, or "mint"... I thought it was cute
-#define         	uMint                       	unsigned __int8                        
-#define         	uShort                         	unsigned short 
-#define				uInt							unsigned int
 
-#define         	MINT_CONFIG_FILE_PATH       	"../../API/config.mints"
-#define         	FAC_CONFIG_FILE_PATH       		"../../API/Faction.fucc"		// "factions-universelles cadres et comment on se citer"... Nothing else
-#define				STATUS_READ_FILE_PATH			"../../API/Keep_Reading.bool"	// This should be read to determine if we should exit or not, determined by a 1 or 0
-#define				NEW_REPORT_FILE_PATH			"../../API/News.report"			// This should be used to update the stage (GUI/Front-End) about any updates!
-#define				WORLD_FILE_PATH					"../../API/map.world"
-#define				WORLD_BACKUP					"../../API/backup.world"
-
-#define         	PRINT_LOW_SEVERITY_ERROR         std::cout << "\nMinor Tomfoolery error"
-#define         	PRINT_MED_SEVERITY_ERROR         std::cout << "\nWell... Shit, Error"
-#define         	PRINT_HIGH_SEVERITY_ERROR        std::cout << "\nOH GOD HELP ME ERROR"
-
-#define         	THIN_LINE                   	printf("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -")
-#define         	THICK_LINE                  	printf("\n###############################################################################################")
-
-#define 			SLEEP(x)						std::this_thread::sleep_for(std::chrono::seconds(x * SPEED_DIVIDER) )
-#define 			NAP(x)							std::this_thread::sleep_for(std::chrono::milliseconds(x * SPEED_DIVIDER) )
-
-#define     		BORING_LOG(x)					if(LOG_LEVEL >= 3){std::cout << "\n" << x ;}
-#define     		INTERESTING_LOG(x)       		if(LOG_LEVEL >= 2){std::cout << "\n" << x ;}
-#define     		CRUCIAL_LOG(x)       			if(LOG_LEVEL >= 1){std::cout << "\n" << x ;}
+#pragma once
 
 
-unsigned int		SPEED_DIVIDER				=	1;		// this is mostly for Speed control... a bigger number means the program will run slower but use up less resources hopefully
-bool				USE_HIGH_PERFORMANCE_MODE	=	false;
-#define				IF_HIGH_PERFORMANCE				if(USE_HIGH_PERFORMANCE_MODE)	// 	My laptop may not be the fastest thing in the world...
-#define				IF_LOW_PERFORMANCE				if(!USE_HIGH_PERFORMANCE_MODE)	//	in fact it's not fast at all actually...
+
+#ifdef _WIN32
+	#include 	<Windows.h>
+	#define 	AM_I_RUNNING_WINDOWS 1
+	#define		DEFAULT_API_FOLDER_PATH "..\\..\\API" 
+
+#else
+	#include 	<unistd.h>
+	#define 	AM_I_RUNNING_WINDOWS 0
+	#define		DEFAULT_API_FOLDER_PATH	"../../API" 
+	
+#endif
+
+#define		NEEDED_THREAD_COUNT	5	
+
+
+
+typedef         	__int8         					mint;	//	micro int, or "mint"... I thought it was cute
+typedef         	unsigned __int8					uMint;                    	                        
+typedef         	unsigned short 					uShort;                       	
+typedef				unsigned int					uInt;						
+
+
+
+#define         	CONFIG_FILE       				"Settings.ALC"			//	Lets get all our details and parametres from here! ( OALC = Open A-Life Config )  
+#define				STATUS_READ_FILE				"Status.stat"			// 	This should be read to determine if we should exit or not, should be determined by a 1 or 0
+#define				WORLD_WRITE_FILE				"writing.world"
+#define				WORLD_READ_FILE					"read.world"
+#define				LOG_FILE						"What_Happened.log"
+#define				FILE_PATH_BUFFER_SIZE			4096
+#define				GENERIC_CHAR_BUFFER_SIZE		3072
+
+
+
+#define         	LOW_SEVERITY_ERROR        "# [ERROR] Minor Tomfoolery error   #"
+#define         	MED_SEVERITY_ERROR        "### [ERROR] Well... Shit, Error   ###"
+#define         	HIGH_SEVERITY_ERROR       "##### [ERROR] OH GOD HELP ME ERROR   #####"
+
+
+
+#define         	THIN_LINE                   	printf("\n- - - - - - - - - - - - - - - - - - - - - - - - - -");
+#define         	THICK_LINE                  	printf("\n###################################################");
+
+
+
+#define 			NAP(x)							Sleep(x * Global_settings.SPEED_DIVIDER)		// 	milliseconds
+#define				SLEEP(x)						Sleep(x * Global_settings.SPEED_DIVIDER * 1000)	//	seconds
+
+
+//#define     		BORING_LOG(x)					if(Global_settings.LOG_LEVEL >= 3){	printf("\n"); printf(x);	}
+
+
+
+
+
+#define				IF_HIGH_PERFORMANCE				if(Global_settings.USE_HIGH_PERFORMANCE_MODE)	// 	My laptop may not be the fastest thing in the world...
+#define				IF_LOW_PERFORMANCE				if(!Global_settings.USE_HIGH_PERFORMANCE_MODE)	//	in fact it's not fast at all actually...
 																				
 																
 
-//	The military structure of a faction could be organised in 3 distinct ways...
-//		1.	Disorganised: 	This would be your Bandits and Mercenaries... No central planning at all.
-//
-//		2.	Migrant:		This would be your Roma and wildlife, if we ever wanted to make them a faction...
-//							these people move constantly to empty spots only
-//
-//		3.	Organised:		Normal Military as we've come to know them, has central planning 
-
-enum	Faction_Structure		{	Disorganised,	Migrant,	Organised	};	
-
+typedef enum	//	Faction Organisational Structures
+{		
+	Centrally_Planned 	= 	'C',		// 	Holds Land, Needs a Home/HQ			(Red Army, US Army)
+	Antonomous			=	'A', 		// 	Holds Land, Doesn't need a Home/HQ	(Wildlife, Bandits)
+	Traders				=	'T',		// 	Don't hold land, Enters into targeted Land secondary Slots 
+	Disease_Like		=	'D'			// 	Don't hold land, Enters into adjacent Land Secondary Slots
+}	
+Faction_Structures;	
 
 
-// 	They're up here to make them publicly accessable from now on
-enum Climates 	{ Cold = 'C', 	Mild = 'M', 	Hot = 'H' 	}; 	
-enum Storms 	{ Blizzard, Terrential_Rain, Heatwave, Emmission }; 
-//	We'll be seeing this mostly in World.h
+
+typedef enum
+{
+	SEVERE_ERROR 	= 1,
+	MILD_ERROR		= 2,
+	MINOR_ERROR		= 3,
+	no_error		= 4
+} Error_Types;
+
+
+
+typedef enum
+{
+	IMPORTANT 	= 1,
+	MILD_IMPORTANCE	= 2,
+	BORING		= 3,
+	no_importance	= 4
+} Importance;
+
+
+
+typedef enum	//	Climate Types
+{ 
+	Cold 	= 	'C',
+	Mild 	= 	'M',
+	Hot 	= 	'H'
+}
+Climates_Types; 	
+
+
+
+typedef enum 	// 	Storm Types
+{ 
+	Blizzard				=	'B', 
+	Terrential_Rain			=	'R', 
+	Heatwave				=	'H', 
+	PSY_Emmission			=	'P',
+	Land_Alter_Emmission	=	'L'	 
+}
+Storms_Types; 
 
 
 
 struct Faction_Settings
 {
-	std::string 		Faction_Name;
-	std::string 		Faction_Initials;
-	uShort 				Faction_Home;
-	uShort 				Starting_Strength;
-	bool				Is_Essential;
-	Faction_Structure	Organisation_Structure;
+	short 						Agression_scale; 	// a scale from -100 to 100... it's a short instead of a mint to prevent over/under flowing... avoid a Gandhi situation ;D
+	char* 						Faction_Name;
+	char* 						Faction_Initials;
+	uShort 						Faction_Home;
+	uShort 						Starting_Strength;
+	bool						Is_Essential;
+	Faction_Structures			Structure;
 };
 
-std::vector<Faction_Settings> Faction_details;
 
 
-/////// 	  Now we getting into some... Undefined stuff...		//////
-uMint  										HOW_MANY_FACTIONS;			//
-uMint   									LOG_LEVEL;					//
-uMint   									WORLD_WIDTH;				//
-uMint   									WORLD_HEIGHT;				//
-uMint										Random_Strength_Range_MAX;	//
-uMint										Random_Strength_Range_MIN;	//
-uShort 										WORLD_SIZE;					//
-//   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -  //
+typedef struct Parametres	// MAKE SURE THAT THERE'S ONLY THE ONE
+{
+	uMint  						HOW_MANY_FACTIONS;
+	uMint   					LOG_LEVEL;
+	uMint   					WORLD_WIDTH;
+	uMint   					WORLD_HEIGHT;
+	uMint						File_ReOpen_Attempts;
+	bool						USE_HIGH_PERFORMANCE_MODE;
+	uShort 						WORLD_SIZE;
+	FILE*						Log_File_Ptr;
+	const char*					API_Folder_Path;
+	uShort						SPEED_DIVIDER;	// this is mostly for Speed control... a bigger number means the program will run slower but use up less resources hopefully
+}	
+Parametres;
 
-	
+struct Parametres Global_settings;
+
+
+
+//	Function Declarations should go here:	- - - - - - - - - - - - - -
+
+void Log_To_File(const char*);
+void Log_To_File(std::string);
+void LOG(const char*, Importance, Error_Types, bool);
+void LOG(const char*, Importance);
+void LOG(const char*);
+void LOG(std::string);
+void LOG(std::string, Importance);
+void LOG(const char*, Error_Types);
+void LOG(std::string, Error_Types);
+Parametres Default_Parametres();
+bool Check_File_Is_Open(FILE*, const char*, const char*, int, bool);
+bool Check_File_Exists(const char*, bool);
+void Get_File_Path(const char*, const char*, char*);
+void Check_Status_File();
+void Log_To_File(const char*);
+void Log_To_File(std::string);
+int Get_Value_From_Settings(const char*);
--- Source/Backend/C++/main.cpp
+++ Source/Backend/C++/main.cpp
@@ -1,25 +1,10 @@
+#include "File_IO.h"
 #include "UTILITY_ITEMS.h"
 
-//		the path of includes should look like this
-//		
-//		            Declarations.h
-//		                 |
-//		              File_IO.h
-//		                 |
-//		               World.h
-//		                 |
-//		            Faction Struct.h
-//		                 |
-//		             Faction AI.h
-//		                 |
-//		            UTILITY Items.h
-//		                 |
-//		               Here :)
-//
-//
-//
+
+
 //			It probably would have made more sense to write everything into the one file 
-//		but oh sweet jesus christ on a tricycle, that would be hell to develop in!
+//		but oh lord, that would be hell to develop in!
 //		I also understand that having mulitple #includes in the one cpp file might've made sense
 //		but in this context this works perfectly fine! :)
 //		
@@ -27,8 +12,6 @@
 //			The purpose of this project is to make an open source version of the
 //		A-Life system in the S.T.A.L.K.E.R video game francise that can be implemented into any 
 //		application that may benefit from it, such as video games and/or visualisers.
-
-
 //
 //
 //			The world is comprised into a 2d grid with different factions, Humans, Wildlife, etc...
@@ -36,9 +19,9 @@
 // 
 //
 //		The code is seperated into 3 clear-cut sections, and named using a restaurant as a metaphor:
-//			1. 	The backend (what you are seeing here, is called the kitchen)
+//			1. 	The backend (what you are seeing here, is the kitchen)
 //			2.	the API (the waitors)
-//			3.	the frontend (What will send in the starting parametres, is called the customers because they will be the ones who see the food)
+//			3.	the frontend (What will send in the starting parametres, are the customers because they will be the ones who see the food)
 //		
 //			The starting parametres should be sent in from the frontend, and read in from our waiters/API files.
 //		The backend/kitchen should then start operation based on those parametres passed in from the Waiters/API
@@ -59,58 +42,35 @@
 
 
 
-int main(int argument_count,	char** API_Folder_Path) 
+int main(int argument_count,	const char** API_Folder_Path_argument) 
 {
-	if (argument_count == 1) 	// No arguments is 1, Giving an arguement is 2+... QUE???
-	{
-		printf("\n We Weren't given an API folder path");
-		std::cin.get();
-		return 1;
-	}
+	// These are our default values!
+	Global_settings = Default_Parametres();
+
+	Log_To_File("Backend Started! Wish us luck!");
+
+	if (argument_count == 1) 	// No arguments given is 1, Giving an arguement is 2+... QUE???
+		{	API_Folder_Path_argument[1] = DEFAULT_API_FOLDER_PATH;	}
+
+	Global_settings.API_Folder_Path = API_Folder_Path_argument[1];
+
+	Log_To_File("Using API Path of:\t┐");
+	Log_To_File(API_Folder_Path_argument[1], false);
 	
-	// Get all of our starting parametres from the Waiters / API files
-    INIT_WORLD(); 
-	
-	
-	// Now lets initialise our threads! 
-	// Each faction gets their own
-	std::vector<std::thread> thr_pool(HOW_MANY_FACTIONS);	
-	
-
-	// call the faction constructor X amount of times
-	ALL_FACTIONS.resize(HOW_MANY_FACTIONS);	
-
-
-	// Make all threads in our pool start the faction AI, we pass in a reference to that pool
-	Startup_Threads(&thr_pool);
-
-
-	// 	Dedicate a thread to just writing to the map
-	std::thread Map_Write_thread(Write_Map_to_Waiters_File);
-
-
-	//	Dedicate a thread to determine if we should still be alive or not!
-	std::thread Stay_Alive_Check(Check_to_See_If_We_Should_Still_Be_Running);
-	//	...and Do NOT continue until the Stay_Alive check finishes or fails
-	Stay_Alive_Check.join();
-
+	// Step 1: What does our .ALC have to say?
+	Get_Set_Global_Settings();
 
 	//////////////////	DEBUGING CODE GOES HERE	//////////////////
+        
 
 
 
 
 
-
-
-
-	
-	HIGH_LOG("yay all done\n\n");
+	printf("\nyay all done... Press enter to exit\n\n");
 
 	//////////////////////////////////////////////////////////////
 
-	printf("\n yay %d", argument_count);
 	std::cin.get();
-		
 	return 0;
 }
