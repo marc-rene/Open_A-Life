a6324b51476595c604c3abce96bba253ec6f46c3
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-04-17 20:44:08 +0100

    Fixed App build .lua

--- Example/Build-App.lua
+++ Example/Build-App.lua
@@ -5,77 +5,74 @@ project "Example"
    targetdir "Binaries/%{cfg.buildcfg}"
    staticruntime "off"
 
-   files {  "Source/**.h",      "Source/**.cpp",    "Source/**.hpp", 
-            "imgui/**.cpp",     "imgui/**.h",       "imgui/**.hpp", 
-            "misc/Styles/**.cpp", "misc/Styles/**.h", "misc/Styles/**.hpp", 
-            "misc/Windows/**.cpp", "misc/Windows/**.h" }
+   files {  "Source/**.h",              "Source/**.cpp",            "Source/**.hpp", 
+            "ThirdParty\\imgui/**.cpp", "ThirdParty\\imgui/**.h",   "ThirdParty\\imgui/**.hpp", 
+            "misc/Styles/**.cpp",       "misc/Styles/**.h",         "misc/Styles/**.hpp", 
+            "misc/Windows/**.cpp",      "misc/Windows/**.h" }
 
    includedirs
    {
         "Source",
-        "imgui",
-        "imgui\\backends",
-
         -- Include Core
         "$(SolutionDir)Core",
         "$(SolutionDir)Core\\Source",
         "$(SolutionDir)Core\\Include",
         "$(SolutionDir)Core\\ThirdParty\\csv\\single_include",
         "$(SolutionDir)Core\\ThirdParty\\spdlog\\include",
-        "$(SolutionDir)Example\\imgui",
-        "$(SolutionDir)Example\\imgui\\backends",
+        "$(SolutionDir)Example\\ThirdParty\\imgui",
+        "$(SolutionDir)Example\\ThirdParty\\imgui\\backends",
         "$(SolutionDir)Example\\misc"
    }
 
 
    removefiles
    {
-    "imgui\\backends\\imgui_impl_allegro5.cpp",  
-    "imgui\\backends\\imgui_impl_allegro5.h",    
-    "imgui\\backends\\imgui_impl_android.cpp",   
-    "imgui\\backends\\imgui_impl_android.h",     
-    "!imgui\\backends\\imgui_impl_dx10.cpp",     -- Keep
-    "!imgui\\backends\\imgui_impl_dx10.h",       -- Keep
-    "!imgui\\backends\\imgui_impl_dx11.cpp",     -- Keep 
-    "!imgui\\backends\\imgui_impl_dx11.h",       -- Keep
-    "imgui\\backends\\imgui_impl_dx12.cpp",      
-    "imgui\\backends\\imgui_impl_dx12.h",        
-    "!imgui\\backends\\imgui_impl_dx9.cpp",      -- Keep
-    "!imgui\\backends\\imgui_impl_dx9.h",        -- Keep
-    "imgui\\backends\\imgui_impl_glfw.cpp",      
-    "imgui\\backends\\imgui_impl_glfw.h",        
-    "imgui\\backends\\imgui_impl_glut.cpp",      
-    "imgui\\backends\\imgui_impl_glut.h",        
-    "imgui\\backends\\imgui_impl_metal.h",       
-    "imgui\\backends\\imgui_impl_metal.mm",      
-    "imgui\\backends\\imgui_impl_opengl2.cpp",   
-    "imgui\\backends\\imgui_impl_opengl2.h", 
-    "imgui\\backends\\imgui_impl_opengl3.cpp",
-    "imgui\\backends\\imgui_impl_opengl3.h",
-    "imgui\\backends\\imgui_impl_opengl3_loader.h",
-    "imgui\\backends\\imgui_impl_osx.h",
-    "imgui\\backends\\imgui_impl_osx.mm",
-    "imgui\\backends\\imgui_impl_sdl2.cpp",
-    "imgui\\backends\\imgui_impl_sdl2.h",
-    "imgui\\backends\\imgui_impl_sdl3.cpp",
-    "imgui\\backends\\imgui_impl_sdl3.h",
-    "imgui\\backends\\imgui_impl_sdlgpu3.cpp",
-    "imgui\\backends\\imgui_impl_sdlgpu3.h",
-    "imgui\\backends\\imgui_impl_sdlrenderer2.cpp",
-    "imgui\\backends\\imgui_impl_sdlrenderer2.h",
-    "imgui\\backends\\imgui_impl_sdlrenderer3.cpp",
-    "imgui\\backends\\imgui_impl_sdlrenderer3.h",
-    "imgui\\backends\\imgui_impl_vulkan.cpp",
-    "imgui\\backends\\imgui_impl_vulkan.h",
-    "imgui\\backends\\imgui_impl_wgpu.cpp",
-    "imgui\\backends\\imgui_impl_wgpu.h",
-    "!imgui\\backends\\imgui_impl_win32.cpp",    -- Keep
-    "!imgui\\backends\\imgui_impl_win32.h",      -- Keep
-    "imgui\\backends\\vulkan/*",
+    "ThirdParty\\imgui\\backends\\imgui_impl_allegro5.cpp",  
+    "ThirdParty\\imgui\\backends\\imgui_impl_allegro5.h",    
+    "ThirdParty\\imgui\\backends\\imgui_impl_android.cpp",   
+    "ThirdParty\\imgui\\backends\\imgui_impl_android.h",     
+    "!ThirdParty\\imgui\\backends\\imgui_impl_dx10.cpp",     -- Keep
+    "!ThirdParty\\imgui\\backends\\imgui_impl_dx10.h",       -- Keep
+    "!ThirdParty\\imgui\\backends\\imgui_impl_dx11.cpp",     -- Keep 
+    "!ThirdParty\\imgui\\backends\\imgui_impl_dx11.h",       -- Keep
+    "ThirdParty\\imgui\\backends\\imgui_impl_dx12.cpp",      
+    "ThirdParty\\imgui\\backends\\imgui_impl_dx12.h",        
+    "!ThirdParty\\imgui\\backends\\imgui_impl_dx9.cpp",      -- Keep
+    "!ThirdParty\\imgui\\backends\\imgui_impl_dx9.h",        -- Keep
+    "ThirdParty\\imgui\\backends\\imgui_impl_glfw.cpp",      
+    "ThirdParty\\imgui\\backends\\imgui_impl_glfw.h",        
+    "ThirdParty\\imgui\\backends\\imgui_impl_glut.cpp",      
+    "ThirdParty\\imgui\\backends\\imgui_impl_glut.h",        
+    "ThirdParty\\imgui\\backends\\imgui_impl_metal.h",       
+    "ThirdParty\\imgui\\backends\\imgui_impl_metal.mm",      
+    "ThirdParty\\imgui\\backends\\imgui_impl_opengl2.cpp",   
+    "ThirdParty\\imgui\\backends\\imgui_impl_opengl2.h", 
+    "ThirdParty\\imgui\\backends\\imgui_impl_opengl3.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_opengl3.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_opengl3_loader.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_osx.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_osx.mm",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdl2.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdl2.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdl3.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdl3.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdlgpu3.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdlgpu3.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdlrenderer2.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdlrenderer2.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdlrenderer3.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_sdlrenderer3.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_vulkan.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_vulkan.h",
+    "ThirdParty\\imgui\\backends\\imgui_impl_wgpu.cpp",
+    "ThirdParty\\imgui\\backends\\imgui_impl_wgpu.h",
+    "!ThirdParty\\imgui\\backends\\imgui_impl_win32.cpp",    -- Keep
+    "!ThirdParty\\imgui\\backends\\imgui_impl_win32.h",      -- Keep
+    "ThirdParty\\imgui\\backends\\vulkan/*",
 
-    "imgui\\examples/**",
-    "imgui\\misc\\freetype/**",
-    "imgui\\misc\\fonts/**.cpp"
+    "ThirdParty\\imgui\\examples/**",
+    "ThirdParty\\imgui\\misc\\freetype/**",
+    "ThirdParty\\imgui\\misc\\fonts/**.cpp"
    }
 
    links
--- Example/misc/Windows/Scenario_Maker_Window.cpp
+++ Example/misc/Windows/Scenario_Maker_Window.cpp
@@ -1,5 +1,4 @@
 ﻿#include "All_Windows.h"
-#include "IMGUI_TEST/imgui_filebrowser.h"
 
 void ImGui::Scenario_Maker_Window(bool* p_open)
 {
@@ -9,10 +8,6 @@ void ImGui::Scenario_Maker_Window(bool* p_open)
         return;
     }
 
-    if (ImGui::Button("Pick file"))
-        ImGui::OpenFileBrowser("C:\\Other\\Git\\Open_A-Life\\Example");
-
-    std::string result;
     
     
   
