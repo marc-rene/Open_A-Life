01165da56b9c70f9946addf9df0348745a0f349d
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-04-17 21:01:00 +0100

    I dont like native file dialogue

--- Example/misc/Windows/Scenario_Maker_Window.cpp
+++ Example/misc/Windows/Scenario_Maker_Window.cpp
@@ -1,4 +1,6 @@
 ﻿#include "All_Windows.h"
+#include <nfd.h>
+#include <stdlib.h>
 
 void ImGui::Scenario_Maker_Window(bool* p_open)
 {
@@ -8,7 +10,28 @@ void ImGui::Scenario_Maker_Window(bool* p_open)
         return;
     }
 
+    NFD_Init();
+
+    nfdu8char_t *outPath;
+    nfdopendialogu8args_t args = {0};
     
+    nfdresult_t result = NFD_OpenDialogU8_With(&outPath, &args);
+    if (result == NFD_OKAY)
+    {
+        puts("Success!");
+        puts(outPath);
+        NFD_FreePathU8(outPath);
+    }
+    else if (result == NFD_CANCEL)
+    {
+        puts("User pressed cancel.");
+    }
+    else 
+    {
+        printf("Error: %s\n", NFD_GetError());
+    }
+
+    NFD_Quit();
     
   
 
