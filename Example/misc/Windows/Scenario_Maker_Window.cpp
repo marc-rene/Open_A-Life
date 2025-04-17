d82463424e26b81866a26a7a12c4cf76be5308f4
marc-rene <95175463+marc-rene@users.noreply.github.com>
2025-04-17 22:31:34 +0100

    SCREW YOU WINDOWS FILE DIALOG

--- Example/misc/Windows/Scenario_Maker_Window.cpp
+++ Example/misc/Windows/Scenario_Maker_Window.cpp
@@ -1,7 +1,8 @@
 ﻿#include "All_Windows.h"
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-// Windows code here
+// Windows code here 
+#include <commdlg.h>
 #else
 // Linux Code here
 #endif
@@ -22,10 +23,8 @@ void ImGui::Scenario_Maker_Window(bool* p_open)
         scenario_file_path = SetSavePath();
         INFO("Scenario Maker Window", "First time running, save path is set to {}", scenario_file_path.string());
     }
-    
-  
 
-    ImGui::Text("Hello :D");
+    ImGui::Text("Current Save path is {}", scenario_file_path.string());
 
     ImGui::End();
 }
@@ -33,5 +32,24 @@ void ImGui::Scenario_Maker_Window(bool* p_open)
 
 std::filesystem::path SetSavePath()
 {
-    return std::filesystem::current_path();
+    char szFile[MAX_PATH] = { 0 };
+
+    OPENFILENAMEA ofn = { 0 };
+    ofn.lStructSize = sizeof(OPENFILENAMEA);
+    ofn.lpstrFile = szFile;
+    ofn.nMaxFile = sizeof(szFile);
+    ofn.lpstrFilter = "Scenario Files (*.scenario)\0*.scenario\0All Files (*.*)\0*.*\0";
+    ofn.nFilterIndex = 1;
+    ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT;
+    ofn.lpstrDefExt = "scenario";
+    ofn.lpTemplateName = "Salut maman";
+    ofn.lpstrTitle = "Habibi, where we save our new Scenario?";
+
+    if (GetSaveFileNameA(&ofn)) {
+        return std::filesystem::path(ofn.lpstrFile);
+    }
+    else {
+        // User cancelled or an error occurred
+        return std::filesystem::current_path() / "Nouveau Scenario.scenario";
+    }
 }
